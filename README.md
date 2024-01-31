982619445316e138e8775ccca7b1fc5f2787f16e
drkchu <derekdbchu@gmail.com>
2024-01-31 11:28:11 -0800

    Get the idea worked out

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # knights-travails
 An algorithm to solve the shortest sequence of moves a knight can move from one position on chess board to another
+
+![Demo Image](./demo.png)
---
+++ solver.js
@@ -0,0 +1,8 @@
+// Think of the start position as the root of a tree, with each branch being a possible path going down.
+
+// Idea: Starting at the start position, add all children to a queue. Essentially a BFS search:
+
+
+function knightMoves(start, finish) {
+
+}
