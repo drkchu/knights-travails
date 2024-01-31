121297f1b7b452286da0f81072ef91239ef0cd41
drkchu <derekdbchu@gmail.com>
2024-01-31 11:28:11 -0800

    Add the template for the algorithm

--- solver.js
+++ solver.js
@@ -3,6 +3,45 @@
 // Idea: Starting at the start position, add all children to a queue. Essentially a BFS search:
 
 
-function knightMoves(start, finish) {
+class pathElement {
+    constructor(movesToGetToPosition, position) {
+        this.movesToGetToPosition = movesToGetToPosition;
+        this.position = position;
+    }
+}
 
+// Example: knightMoves([0,0],[1,2]) == [[0,0],[1,2]]
+function knightMoves(start, finish) {
+    const queue = [];
+    let result = [];
+
+    queue.push(new pathElement([start], start));
+
+    while (queue.length !== 0) {
+        const currPathElement = queue.shift();
+
+        if (currPathElement.position === finish)
+            return [];
+        
+        const nextMoves = generateNextMoves(currPathElement.position);
+
+        nextMoves.forEach((position) => {
+            if (isValidBoardPosition(position)) {
+                queue.push(new pathElement(currPathElement.movesToGetToPosition.concat(position), position));
+            }
+        });
+    }
+}
+
+// Returns an array of all the next possible moves from the given positon, valid or invalid
+function generateNextMoves(position) {
+    return [];
+}
+
+// Function that checks wherter the given position is valid
+function isValidBoardPosition(position) {
+    const x = position[0];
+    const y = position[1];
+
+    return 0 <= x && x <= 7 && 0 <= y && y <= 7;
 }
